d4242b59e40a45ea6804c86514fbb884246f9473
mativiscusso <mativiscusso@gmail.com>
2019-09-20 11:23:54 -0300

    acomodando grafica

--- registro.php
+++ registro.php
@@ -14,35 +14,8 @@
     <title>Burn Quiz | REGISTRO</title>
 </head>
 <body>
-<header> 
-  <nav id="menu" class="navbar navbar-expand-lg navbar-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span id="iconboton" class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                  <a class="nav-link" href="index.php">HOME</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">FAQs</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="registro.php">REGISTRO</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">LOGIN</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">CONTACTO</a>
-                </li>  
-              </ul>
-              <span class="navbar-text">
-                <img src="img/burnquiz_logow.png" width="10%" alt="">
-              </span>
-            </div>
-          </nav>
-   </header>
+  <?php include("header.php"); ?>
+
         <div id="registro" class="container-fluid">
         <h1>REGISTRO</h1>   
         <form action="validar.php" method="POST" class="needs-validation" novalidate>
@@ -143,11 +116,8 @@
           </script>
       </div>
     </div>
-    <footer>
-          <div id="contacto" class="container-fluid py-3">
-            <span>BURN QUIZ | Un juego para pensar</span>
-            
-          </div>
-    </footer>
+
+<?php include("footer.php"); ?>
+
 </body>
 </html>
