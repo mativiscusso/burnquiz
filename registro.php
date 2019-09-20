315975874ee5f20387ec3b4952b5e38fdd0b8d58
Nacho98Rod <nacho-rodriguez98@hotmail.com>
2019-09-20 14:45:16 -0300

    minimos cambios

--- registro.php
+++ registro.php
@@ -46,66 +46,66 @@
         <div id="registro" class="container-fluid">
         <h1>REGISTRO</h1>   
         <form action="validar.php" method="POST" class="needs-validation" novalidate>
-            <div class="form-row">
-              <div class="col-md-4 mb-3">
-                <label for="validationCustom01">Nombre</label>
-                <input type="text" class="form-control" id="validationCustom01" placeholder="Nombre" required>
-                <div class="valid-feedback">
-                  Se ve bien!
-                </div>
-              </div>
-              <div class="col-md-4 mb-3">
-                <label for="validationCustom02">Apellido</label>
-                <input type="text" class="form-control" id="validationCustom02" placeholder="Apellido" required>
-                <div class="valid-feedback">
+          <div class="form-row">
+            <div class="col-md-4 mb-3">
+              <label for="validationCustom01">Nombre</label>
+              <input type="text" class="form-control" id="validationCustom01" name="nombre" placeholder="Nombre" required>
+              <div class="valid-feedback">
                 Se ve bien!
-                </div>
               </div>
-              <div class="col-md-4 mb-3">
-                <label for="validationCustomUsername">Usuario</label>
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroupPrepend">@</span>
-                  </div>
-                  <input type="text" class="form-control" id="validationCustomUsername" placeholder="Usuario" aria-describedby="inputGroupPrepend" required>
-                  <div class="invalid-feedback">
-                    Por favor elige un nombre de usuario valido.
+            </div>
+            <div class="col-md-4 mb-3">
+              <label for="validationCustom02">Apellido</label>
+              <input type="text" class="form-control" id="validationCustom02" name="apellido" placeholder="Apellido" required>
+              <div class="valid-feedback">
+              Se ve bien!
+              </div>
+            </div>
+            <div class="col-md-4 mb-3">
+              <label for="validationCustomUsername">Usuario</label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="inputGroupPrepend">@</span>
+                </div>
+                <input type="text" class="form-control" id="validationCustomUsername" name="usuario" placeholder="Usuario" aria-describedby="inputGroupPrepend" required>
+                <div class="invalid-feedback">
+                  Por favor elige un nombre de usuario valido.
                   </div>
                 </div>
               </div>
             </div>
             <div class="form-group">
-                <label for="inputPassword6">Contraseña</label>
-                <input type="password" id="inputPassword6" class="form-control mx-sm-3" aria-describedby="passwordHelpInline">
-                <small id="passwordHelpInline" class="text-muted">
-                  Debe tener entre 8-20 caracteres.
-                </small>
+              <label for="inputPassword6">Contraseña</label>
+              <input type="password" id="inputPassword6" class="form-control mx-sm-3" name="contra" aria-describedby="passwordHelpInline">
+              <small id="passwordHelpInline" class="text-muted">
+                Debe tener entre 8-20 caracteres.
+              </small>
+            </div>
+            <div class="form-group" id="contra2">
+              <label for="inputPassword6">Repetir Contraseña</label>
+              <input type="password" id="inputPassword6" class="form-control mx-sm-3" name="rcontra" aria-describedby="passwordHelpInline">
+              <small id="passwordHelpInline" class="text-muted">
+                Debe tener entre 8-20 caracteres.
+              </small>
+            </div>
+          <div class="form-row">
+            <div class="col-md-6 mb-3">
+              <label for="validationCustom03">Ciudad</label>
+              <input type="text" class="form-control" id="validationCustom03" name="ciudad" placeholder="Ciudad" required>
+              <div class="invalid-feedback">
+                  Por favor elige una ciudad valida.
               </div>
-              <div class="form-group" id="contra2">
-                <label for="inputPassword6">Repetir Contraseña</label>
-                <input type="password" id="inputPassword6" class="form-control mx-sm-3" aria-describedby="passwordHelpInline">
-                <small id="passwordHelpInline" class="text-muted">
-                  Debe tener entre 8-20 caracteres.
-                </small>
-              </div>
-            <div class="form-row">
-              <div class="col-md-6 mb-3">
-                <label for="validationCustom03">Ciudad</label>
-                <input type="text" class="form-control" id="validationCustom03" placeholder="Ciudad" required>
-                <div class="invalid-feedback">
-                    Por favor elige una ciudad valida.
-                </div>
-              </div>
-              <div class="col-md-3 mb-3">
-                <label for="validationCustom04">Provincia</label>
-                <input type="text" class="form-control" id="validationCustom04" placeholder="Provincia" required>
+            </div>
+            <div class="col-md-3 mb-3">
+              <label for="validationCustom04">Provincia</label>
+              <input type="text" class="form-control" id="validationCustom04" name="provincia" placeholder="Provincia" required>
                 <div class="invalid-feedback">
                     Por favor elige una provincia valida.
                 </div>
               </div>
               <div class="col-md-3 mb-3">
                 <label for="validationCustom05">País</label>
-                <input type="text" class="form-control" id="validationCustom05" placeholder="País" required>
+                <input type="text" class="form-control" id="validationCustom05" name="pais" placeholder="País" required>
                 <div class="invalid-feedback">
                   Por favor elige un país valido.
                 </div>
@@ -120,29 +120,12 @@
                 <div class="invalid-feedback">
                   Tienes que aceptar antes de enviar.
                 </div>
-              </div>
+            <button type="submit" style="margin-left: 20px; background-color: rgb(165,0,0); border-color: rgb(165,0,0);" class="btn btn-primary">Registrarse</button>
+            </form>
             </div>
-          <script>
-          (function() {
-            'use strict';
-            window.addEventListener('load', function() {
-              // Fetch all the forms we want to apply custom Bootstrap validation styles to
-              var forms = document.getElementsByClassName('needs-validation');
-              // Loop over them and prevent submission
-              var validation = Array.prototype.filter.call(forms, function(form) {
-                form.addEventListener('submit', function(event) {
-                  if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                  }
-                  form.classList.add('was-validated');
-                }, false);
-              });
-            }, false);
-          })();
-          </script>
-      </div>
-    </div>
+          </div>
+        </div>
+        </div>
     <footer>
           <div id="contacto" class="container-fluid py-3">
             <span>BURN QUIZ | Un juego para pensar</span>
