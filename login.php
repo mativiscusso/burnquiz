4efd3adf9a16f85a8788938ab06d8789d8353512
mativiscusso <mativiscusso@gmail.com>
2019-09-23 13:35:52 -0300

    persistencia formularios+validacion Contacto

--- login.php
+++ login.php
@@ -1,3 +1,17 @@
+<?php
+$usuario = '';
+$pass = '';
+if ($_POST) {
+    $usuario = $_POST['usuario'];
+    $pass = $_POST['pass'];
+}
+
+include ('validar.php');
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,21 +32,21 @@
 <body>
     <?php include("header.php"); ?>
     
-<div id="portada" class= "container-fluid">
-        <form class="container" action="validar.php" method="get">
-                <h1>Login</h1>
-                
-            <input class="completar" type="text" name="name" id="" placeholder="correo electronico"><br>
-            <input class="completar" type="password" name="pass" id="" placeholder="contraseña"><br>
-            <input class="check" type="checkbox" name="" id="">
-            <div class="a2" ><a href=""><p></p> Olvido su cotraseña o email?</p></a></div>
-            <div class="a2" ><a href=""><p></p> Aún no se ha registrado?</p></a></div>
-            <div class="btn"><input type="submit" value="Iniciar Sesión"><br></div>
-            <div class="parrafo"><p>Al hacer clic en Iniciar sesión, confirmo que he leído y acepto los Términos de servicio y la Política de privacidad de  ESTE JUEGO INCREIBLE</p></div>
-       </form>
-</div>  
+        <div id="portada" class= "container-fluid">
+                <form class="container" action="login.php" method="get">
+                        <h1>Login</h1>
+                        
+                    <input class="completar" type="text" name="usuario" id="" placeholder="Email"><br>
+                    <input class="completar" type="password" name="pass" id="" placeholder="Contraseña"><br>
+                    <input class="check" type="checkbox" name="" id="">
+                    <div class="a2" ><a href=""><p></p> Olvido su cotraseña o email?</p></a></div>
+                    <div class="a2" ><a href=""><p></p> Aún no se ha registrado?</p></a></div>
+                    <div class="btn"><input type="submit" value="Iniciar Sesión"><br></div>
+                    <div class="parrafo"><p>Al hacer clic en Iniciar sesión, confirmo que he leído y acepto los Términos de servicio y la Política de privacidad de  ESTE JUEGO INCREIBLE</p></div>
+            </form>
+        </div>  
     
-<?php include("footer.php"); ?>
+    <?php include("footer.php"); ?>
   
 </body>
 </html>
