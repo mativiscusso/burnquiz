a22a59dba73395d35ba245d773ee4fb520f8164c
mativiscusso <mativiscusso@gmail.com>
2019-10-15 16:43:54 -0300

    boton Start aparece Logueado

--- index.php
+++ index.php
@@ -15,13 +15,17 @@ include_once('validar.php');
             </div>
             <div id="right" class="col-12 col-md-6 py-2">
                 <img src="img/burnquiz_logo.png"  alt=""><br>
+                <?php if(isLogged()) { 
+                ?> 
                 <input type="checkbox" id="cb1" /><label id="cb2" for="cb1"><a id="start" href="juego.php">Start</a></label>
+                <?php
+                    }
+                ?>
             </div>
         </main>
     </div>
     
-        <section>
-            <!-- descripcion -->
+        <section class="container" id="descripcion">
             <p class="descripcion">Burn Quiz es un juego desarrollado con el fin de ser presentado como trabajo integrador en el curso de Diseño Web FullStack dictado por Digital House.</p>
             <p class="descripcion">La trivia estara compuesta por preguntas capciosas o de pensamiento logico, las cuales tendran 3 posibles respuestas. El objetivo del juego es acumular la mayor cantidad de preguntas acertadas en un lapso de tiempo determinado.</p>
         </section>
