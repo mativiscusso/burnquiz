6c3dae720fea59a46f4e76329dbf782d3f3d8c7d
mativiscusso <mativiscusso@gmail.com>
2019-12-02 14:35:25 -0300

    acomodando carpetas

--- DGJuego_version_DB/pyr.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once('validar.php');
-require_once('clases/CargaPreguntas.php');
--- DGJuego_version_JSON/pyr.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $fileContent = file_get_contents("files/preguntas.json");
-    $pyrArray = json_decode($fileContent, true);
-if ($_POST) {
-    $arrayNuevo = $_POST;
-    $pyrArray[] = $arrayNuevo;
-    file_put_contents("files/preguntas.json", json_encode($pyrArray));
-    header('location: abm.php');exit;
-}
