c5f3d4f5e497030ab32647ed92f11182796efe3d
mativiscusso <mativiscusso@gmail.com>
2019-12-17 20:10:07 -0300

    pagina resultado

--- resources/views/burnquiz/resultado.blade.php
+++ resources/views/burnquiz/resultado.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div id="portada" class="container py-5">
             <h1><?= "EL JUEGO HA TERMINADO"?></h1>
-            <h2>USUARIO:</h2>
-            <h2>SU PUNTAJE ES: </h2><br>
-            <input type="checkbox" id="cb1" /><label id="cb2" for="cb1"><a id="start" href="/juego">Juego Nuevo</a></label>
+            <h2>USUARIO: {{ Auth::user()->user }}</h2>
+            <h2>SU PUNTAJE ES: {{ session()->get('puntaje', 0) }} </h2><br>
+            <input type="checkbox" id="cb1" /><label id="cb2" for="cb1"><a id="start" href="/juego" onclick="{{session(['puntaje' => 0])}}">Juego Nuevo</a></label>
 </div>
 
 @endsection
